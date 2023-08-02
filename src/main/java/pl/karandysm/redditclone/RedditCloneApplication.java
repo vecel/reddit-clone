bbf15caeed0caedde7fbbd4b4c83c225611354f1
vecel <mateusz.karandys@gmail.com>
2023-08-02 15:28:17 +0200

    initial commit

---
+++ src/main/java/pl/karandysm/redditclone/RedditCloneApplication.java
@@ -0,0 +1,13 @@
+package pl.karandysm.redditclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditCloneApplication.class, args);
+	}
+
+}
