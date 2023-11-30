397a5dd6eeee37f191b2465cb20ebf262eb60410
vecel <mateusz.karandys@gmail.com>
2023-11-30 20:50:00 +0100

    user logout

--- src/main/java/pl/karandysm/redditclone/controller/IndexController.java
+++ src/main/java/pl/karandysm/redditclone/controller/IndexController.java
@@ -3,12 +3,20 @@ package pl.karandysm.redditclone.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class IndexController {
 	
 	@GetMapping("/")
 	public String index() {
 		return "index";
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.removeAttribute("user");
+		return "redirect:/";
 	}
 
 }
